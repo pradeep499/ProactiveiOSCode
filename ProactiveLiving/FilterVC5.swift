4010c85ba854d0330b3585ba1b6e163b13b03d1b
Mohd Asim <mohd.asim@affle.com>
2017-03-15 15:27:46 +0530

    Addition and Deletion resource

---
+++ ProactiveLiving/FilterVC5.swift
@@ -0,0 +1,35 @@
+//
+//  FilterVC5.swift
+//  ProactiveLiving
+//
+//  Created by Mohd Asim on 09/03/17.
+//  Copyright © 2017 appstudioz. All rights reserved.
+//
+
+import UIKit
+
+class FilterVC5: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
