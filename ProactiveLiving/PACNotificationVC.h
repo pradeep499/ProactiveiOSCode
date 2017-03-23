66ac5cb346592457a1bb9756a721e573351b739b
Mohd Asim <mohd.asim@affle.com>
2017-03-23 17:45:53 +0530

    PAC Notification screen added

---
+++ ProactiveLiving/PACNotificationVC.h
@@ -0,0 +1,13 @@
+//
+//  PACNotificationVC.h
+//  ProactiveLiving
+//
+//  Created by Mohd Asim on 23/03/17.
+//  Copyright © 2017 appstudioz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PACNotificationVC : UIViewController
+
+@end
---
+++ ProactiveLiving/PACNotificationVC.m
@@ -0,0 +1,37 @@
+//
+//  PACNotificationVC.m
+//  ProactiveLiving
+//
+//  Created by Mohd Asim on 23/03/17.
+//  Copyright © 2017 appstudioz. All rights reserved.
+//
+
+#import "PACNotificationVC.h"
+
+@interface PACNotificationVC ()
+
+@end
+
+@implementation PACNotificationVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
